448a98656dd34e41a473bf2d238eee6c5a7aa417
rowburt <32477930+rowburt@users.noreply.github.com>
2024-09-18 16:27:35 +0200

    Uninstalled GNOME Boxes & Weather

--- host/modules/flatpak.nix
+++ host/modules/flatpak.nix
@@ -7,13 +7,11 @@
     packages = [
       "com.github.tchx84.Flatseal"
       "com.jetbrains.Rider"
-      "org.gnome.Boxes"
       "org.gnome.Calculator"
       "org.gnome.clocks"
       "org.gnome.Evince"
       "org.gnome.Loupe"
       "org.gnome.TextEditor"
-      "org.gnome.Weather"
       "org.libreoffice.LibreOffice"
     ];
   };
