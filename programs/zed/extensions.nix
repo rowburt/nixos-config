3e9fb4bb0cd4faaf9b017ccbad927626c2aea22b
rowburt <32477930+rowburt@users.noreply.github.com>
2024-12-29 22:02:25 +0100

    Updated Zed configuration

--- programs/zed/extensions.nix
+++ programs/zed/extensions.nix
@@ -5,11 +5,14 @@
     extensions = [
       # "csharp"
       "csv"
+      "dart"
+      # "haskell"
       "http"
       "java"
       "kotlin"
       "neocmake"
       "nix"
+      "qml"
       "sql"
       "toml"
       "xml"
@@ -19,14 +22,43 @@
       clang-tools
       cmake
       gradle
+      kdePackages.qtdeclarative
+      ktfmt
       neocmakelsp
       nixd
+      nixfmt-rfc-style
     ];
-    
+
     userSettings = {
+      lsp = {
+        clangd.arguments = [ "-background-index" "-compile-commands-dir=build" ];
+        dart.options.lineLength = 140;
+
+        # https://github.com/fwcd/kotlin-language-server/blob/main/server/src/main/kotlin/org/javacs/kt/Configuration.kt
+        kotlin-language-server.settings = {
+          inlayHints = {
+            typeHints = true;
+            parameterHints = true;
+            chainedHints = true;
+          };
+
+          formatting.ktfmt.maxWidth = 140;
+        };
+
+        # TODO: Configure language servers
+        nixd.options.autoArchive = true;
+      };
+
       languages = {
         # Only use nixd for nix files
-        Nix.language_servers = [ "nixd" "!nil" ];
+        Nix.language_servers = [
+          "nixd"
+          "!nil"
+        ];
+      };
+
+      file_types = {
+        CMake = ["CMakeLists.txt"];
       };
     };
   };
