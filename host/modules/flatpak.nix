5b501cfa3158ec8be3252a8b1a932f581af2f3ee
rowburt <32477930+rowburt@users.noreply.github.com>
2025-03-18 17:57:13 +0100

    Switched from Evince to Papers

--- host/modules/flatpak.nix
+++ host/modules/flatpak.nix
@@ -10,8 +10,8 @@
       "org.cvfosammmm.Setzer"
       "org.gnome.Calculator"
       "org.gnome.clocks"
-      "org.gnome.Evince"
       "org.gnome.Loupe"
+      "org.gnome.Papers"
       "org.gnome.TextEditor"
       "org.onlyoffice.desktopeditors"
     ];
