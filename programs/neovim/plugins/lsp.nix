3e2d608ca8d62e08ea3420760d947907a222bf45
rowburt <32477930+rowburt@users.noreply.github.com>
2025-08-05 23:53:29 +0200

    Enabled neovim json & ts lsps

--- programs/neovim/plugins/lsp.nix
+++ programs/neovim/plugins/lsp.nix
@@ -12,10 +12,12 @@
   # TODO: Configure LSPs
   servers = {
     dartls.enable = true;
+    jsonls.enable = true;
     kotlin_language_server.enable = true;
     lua_ls.enable = true;
     nixd.enable = true;
     pyright.enable = true;
+    ts_ls.enable = true;
 
     rust_analyzer = {
       enable = true;
